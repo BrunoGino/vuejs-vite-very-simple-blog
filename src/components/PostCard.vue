50c43ecb53c63a99983107cbf477e37ea37455fa
BrunoGino <bruno.gino24@gmail.com>
2025-05-01 11:41:57 -0300

    feat: wrapping card with router-link

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,26 +1,14 @@
 <template>
-  <v-card title="Dialog">
-    <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </v-card-text>
-    <v-card-actions>
-        <v-spacer></v-spacer>
 
-        <v-btn
-          text="Close Dialog"
-          @click="isActive.value = false"
-        ></v-btn>
-      </v-card-actions>
-  </v-card>
-  <div class="mb-6 p-4 border rounded-lg shadow hover:shadow-lg transition">
-    <router-link :to="`/post/${post.slug}`">
-      <h2 class="text-xl font-semibold text-blue-600 hover:underline">
-        {{ post.title }}
-      </h2>
-    </router-link>
-    <p class="text-gray-700">{{ post.description }}</p>
-    <hr />
-  </div>
+  <router-link :to="`/post/${post.slug}`">
+    <v-card :title="`${post.title}`">
+
+      <v-card-text>
+        {{ post.description }}
+      </v-card-text>
+
+    </v-card>
+  </router-link>
 </template>
 
 <script setup>
