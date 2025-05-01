5638b1ba7ca8287d029f893ba39760013d77aa26
BrunoGino <bruno.gino24@gmail.com>
2025-05-01 11:32:54 -0300

    chore: adding vuetify

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,4 +1,17 @@
 <template>
+  <v-card title="Dialog">
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </v-card-text>
+    <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+  </v-card>
   <div class="mb-6 p-4 border rounded-lg shadow hover:shadow-lg transition">
     <router-link :to="`/post/${post.slug}`">
       <h2 class="text-xl font-semibold text-blue-600 hover:underline">
